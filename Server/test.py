a16443a62dbc3c9682df2689d4c7372edb32570c
Unknown <finnjoana56@gmail.com>
2019-04-15 12:16:44 +0200

    test connect to Server

--- Server/test.py
+++ Server/test.py
@@ -1,4 +1,6 @@
 import requests
-res = requests.post('http://localhost:5000/login', json={"username":"krypto", "password":"koffer"})
+#res = requests.post('http://localhost:5000/login', json={"username":"krypto", "password":"koffer"})
+res = requests.post('http://safe-harbour.de:4242/login', json={"username":"krypto", "password":"koffer"})
+
 if res.ok:
     print(res.text)
