5919433c7ee730f9cbfff428ac3dd1f9baba316c
Finn Lenz <finnjoana56@gmail.com>
2019-09-09 20:33:34 +0200

    Refactor

--- Server/test.py
+++
@@ -1,6 +0,0 @@
-import requests
-#res = requests.post('http://localhost:5000/login', json={"username":"krypto", "password":"koffer"})
-res = requests.post('http://localhost:5000/deleteRecordAPI', json={"recordId":"3"})
-
-if res.ok:
-    print(res.text)
